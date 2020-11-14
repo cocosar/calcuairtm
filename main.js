000769c794ca44f59dd8eaf5366d77b0be6902ba
Juan José Sarsotti <cocosarsotti@gmail.com>
2020-11-14 17:33:31 -0300

    terminado el calculo automatico

--- main.js
+++ main.js
@@ -3,23 +3,60 @@
 const inputData = document.querySelector('input');
 const btnMain = document.querySelector('.main');
 const btnReset = document.querySelector('.reset');
+const result = document.querySelector('.amount');
+const resultRate = document.querySelector('.amount-rate');
+const totalContainer = document.querySelectorAll('.total')
+
+
+//Declarations
+const mainUrl = 'https://rates.airtm.io/air-rates'
+
+// Payoneer USD -> air-rates ( {USD.methods[87] } )
+// Peso arg -> air-rates ( { ARS.methosds[0] } )
+
 
 // Valores 
+const AIRTM_FEE = 0.4;
 
-// const rates = {
-//     ARS_COMPRA = 151.51,
-//     ARS_VENTA = 142.93,
-//     USD_COMPRA = 1.049,
-//     USD_VENTA = 0.96
-// };
+const USD_COMPRA = 1.049;
+const USD_VENTA = 0.96;
+const ARS_COMPRA = 165.07;
+const ARS_VENTA = 155.73;
 
-// Function
+
+/* Calculate */
 const calculate = () => {
-    console.log(1+1)
+    
+    let usd_value = inputData.value;
+    
+    if(usd_value) {
+        let usd_per_airtm = 1/USD_COMPRA;
+        let airtm_buyed = usd_value * usd_per_airtm;
+        airtm_buyed -= AIRTM_FEE * 2;
+        let ars_buyed = airtm_buyed * ARS_VENTA; 
+        let rounded_ars = Math.round(ars_buyed * 100) / 100;
+        let rounded_rate = Math.round(ars_buyed/usd_value * 100) / 100;
+        
+        result.innerText = `$ ${rounded_ars.toLocaleString()}`;
+        resultRate.innerText = `$ ${rounded_rate.toLocaleString()}`;
+        totalContainer.forEach(element => {
+            element.setAttribute('style', 'display: block')
+        });
+    }
+
 }
 
-btnMain.onclick = calculate;
+const reset = () => {
+    totalContainer.forEach(element => {
+        element.setAttribute('style', 'display: none')
+    });
+    inputData.value = '';
+}
 
+/* On enter event */
 
-console.log(inputData.value)
-
+inputData.onkeyup = (e)=> {
+    if (e.keyCode === 13) {
+        calculate();
+    }
+}
