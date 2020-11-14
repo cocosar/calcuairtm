06ae0b54e8818976eb6ebce53bbd1f563095d97e
Juan José Sarsotti <cocosarsotti@gmail.com>
2020-11-14 15:56:09 -0300

    iniciando

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// Selectores
+const inputData = document.querySelector('input');
+const btnMain = document.querySelector('.main');
+const btnReset = document.querySelector('.reset');
+
+// Valores 
+
+// const rates = {
+//     ARS_COMPRA = 151.51,
+//     ARS_VENTA = 142.93,
+//     USD_COMPRA = 1.049,
+//     USD_VENTA = 0.96
+// };
+
+// Function
+const calculate = () => {
+    console.log(1+1)
+}
+
+btnMain.onclick = calculate;
+
+
+console.log(inputData.value)
+
